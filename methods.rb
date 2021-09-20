59d5b372a55b6825714bfcc1a6c32090cc862349
roger chat <mattrogerpelosi@gmail.com>
2021-09-20 10:10:48 -0400

    commit tree afe382cd5cd99eb26181d759bf0c627636e65180

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
-# Your code here!
+#####1
+def greet_programmer 
+    puts "Hello, programmer!"
+end 
+#####2
+def greet name
+    puts "Hello, #{name}!"
+end 
+#####3
+def greet_with_default name='programmer'
+    puts "Hello, #{name}!"
+end 
+#####4
+def add num1, num2
+    num1 + num2
+end
+#####5
+def halve num
+    if num.class != Integer
+        return nil
+    end
+
+    num/2
+end 
+
